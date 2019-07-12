58cac1146c00ec45e6aeab24a61c299e80ba82ed
Thomas Weller <thomas.weller@mitutoyo-ctl.de>
2019-07-12 09:59:55 +0200

    Integrate downloading of fonts in installation script

--- install.sh
+++ install.sh
@@ -183,3 +183,12 @@ fi
 if [ "$connect_usbsound" = "yes" ]; then
     echo -e "\e[91mThe USB sound card seems not connected. Please connect.\e[39m";
 fi
+
+echo "FontAwesome download..."
+wget http://use.fontawesome.com/releases/v5.8.1/fontawesome-free-5.8.1-web.zip -O fontawesome-free-web.zip
+echo "Unzip Fontawesome"
+unzip fontawesome-free-web.zip -d ./static/
+echo "Remove downloaded zip file..."
+mv ./static/fontawesome-free-5.8.1-web ./static/fontawesome-free-web
+rm fontawesome-free-web.zip
+cd ..
