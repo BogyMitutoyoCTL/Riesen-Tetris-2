9db9f6e0ccf802c7b23017b5b7d79a8606f7f5a5
StrgV <timporgertim@gmail.com>
2019-07-16 15:06:29 +0200

    Farben auf integer geändert.

--- Colors.py
+++ Colors.py
@@ -1,22 +1,21 @@
 class Color:
     def __init__(self, red, green, blue):
-
         self.red = red
         self.green = green
         self.blue = blue
 
+
 class Block_color:
+    darkblue = Color(0, 0, 139)
 
-    darkblue = Color(0.0008, 0, 53.3)
+    yellow = Color(255, 255, 0)
 
-    yellow = Color(98.82, 100, 16.08)
+    green = Color(0, 128, 0)
 
-    green = Color(0, 94.51, 1.18)
+    red = Color(255, 0, 0)
 
-    red = Color(100, 14.51, 0)
+    pink = Color(255, 20, 174)
 
-    pink = Color(94.12, 5.49, 100)
+    orange = Color(255, 140, 0)
 
-    orange = Color(100, 62.75, 3.14)
-
-    turquoise = Color(0, 97.25, 97.65)
+    turquoise = Color(135, 206, 250)
