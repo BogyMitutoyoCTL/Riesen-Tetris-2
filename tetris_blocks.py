6d05ac05d383dd81da953dbdf84b4ae900b379b4
JoelHiller <joel.david.hiller@gmail.com>
2019-07-16 13:35:12 +0200

    Random Blocks

--- tetris_blocks.py
+++ tetris_blocks.py
@@ -14,7 +14,6 @@ class Block:
             print(x)
 
 class Blocktype:
-
     t = [[0, 0, 0, 0],
          [0, 0, 0, 0],
          [0, 1, 0, 0],
@@ -50,12 +49,9 @@ class Blocktype:
                [0, 0, 1, 0],
                [0, 0, 1, 1]]
 
-a = Block(Blocktype.t, Block_color.pink)
-b = Block(Blocktype.z_left, Block_color.red)
-c = Block(Blocktype.z_right, Block_color.turquoise)
-d = Block(Blocktype.i, Block_color.green)
-e = Block(Blocktype.square, Block_color.yellow)
-f = Block(Blocktype.l_left, Block_color.darkblue)
-g = Block(Blocktype.l_right, Block_color.orange)
-
-
+block_list = [Block(Blocktype.t, Block_color.pink),
+              Block(Blocktype.z_left, Block_color.red),
+              Block(Blocktype.z_right, Block_color.turquoise),
+              Block(Blocktype.i, Block_color.green),
+                Block(Blocktype.square, Block_color.yellow),
+                Block(Blocktype.l_left, Block_color.darkblue)]
