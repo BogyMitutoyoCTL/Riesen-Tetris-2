feb2c7435ca6d8a2594e8c763ab855d577c68ea4
JoelHiller <joel.david.hiller@gmail.com>
2019-07-17 08:44:43 +0200

    änderung mit Rückgabe

--- controller.py
+++ controller.py
@@ -1,6 +1,8 @@
 import time
-
+import tetris_blocks
 import pygame
+import Colors
+
 class Controller:
 
 
@@ -8,37 +10,47 @@ class Controller:
 
         pygame.init()
         pygame.joystick.init()
-        jc = pygame.joystick.get_count()
-        print(jc)
 
 
         a = pygame.joystick.Joystick(0)
         a.init()
-        while True:
-            pygame.event.get()
-            if a.get_button(1)>0.001:
-                print("Rechts drehen!")
+        pygame.event.get()
+        if a.get_button(1)>0.001:
+            print("Rechts drehen!")
+            right = "Right!"
+            return right
 
 
-            if a.get_button(0)>0.001:
-                print("links drehen!")
+        if a.get_button(0)>0.001:
+            print("links drehen!")
+            e = tetris_blocks.Block(tetris_blocks.Blocktype.t, Colors.Block_color.yellow)
+            e.rotation(-1)
+            left = "Left!"
+            return left
 
 
-            if a.get_button(7)>0.001:
-                print("Spiel beenden!")
+        if a.get_button(7)>0.001:
+            print("Spiel beenden!")
+            end = "End!"
+            return end
 
 
-            if a.get_axis(0)<-0.001:
-                print("Gehe nach Links!")
+        if a.get_axis(0)<-0.001:
+            print("Gehe nach Links!")
+            go_left = "Go Left!"
+            return go_left
 
 
-            if a.get_axis(0)>0.001:
-                print("Gehe nach Rechts!")
+        if a.get_axis(0)>0.001:
+            print("Gehe nach Rechts!")
+            go_right = "Go Right!"
+            return go_right
 
 
-            if a.get_axis(1)>0.001:
-                print("Gehe nach unten!")
-
+        if a.get_axis(1)>0.001:
+            print("Gehe nach unten!")
+            go_down =  "Go Down!"
+            return  go_down
 
 
             time.sleep(0.05)
