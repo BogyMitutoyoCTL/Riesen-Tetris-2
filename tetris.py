015ca29a70958018258749dd26044c2069db8965
StrgV <timporgertim@gmail.com>
2019-07-17 14:19:04 +0200

    BlÖcKe FaLlEn :O

--- tetris.py
+++ tetris.py
@@ -19,70 +19,74 @@ a.init()
 
 g = controller.Controller(a)
 
-dev = neopixel(width=10, height=20, rotate=0, mapping=drawer.HAT)
+color_canvas = neopixel(width=10, height=20, rotate=0, mapping=drawer.HAT)
 
 serial = spi(port=0, device=0, gpio=noop())
-dev2 = max7219(serial, cascaded=4, block_orientation=90,
-               rotate=0, blocks_arranged_in_reverse_order=True)
+red_canvas = max7219(serial, cascaded=4, block_orientation=90,
+                     rotate=0, blocks_arranged_in_reverse_order=True)
 
-play = playground.Playground(20, 10)
-play.draw()
+color_playground = playground.Playground(20, 10)
+color_playground.draw()
 
-play2 = playground.Playground(8, 32)
-play2.draw()
+red_playground = playground.Playground(8, 32)
+red_playground.draw()
 
 rand = random_blocks.Randomblock()
-blo = rand.get_random_block()
-blo3 = rand.get_random_block()
-blo2 = blo3.strech_block_twice()
+current_block = rand.get_random_block()
+next_block = rand.get_random_block()
+preview_block = next_block.strech_block_twice()
 
-Drawer1 = drawer.draw(dev)
+color_drawer = drawer.draw(color_canvas)
 
-blo.draw_block()
-Drawer1.draw_playground(play)
-Drawer2 = littlemonitor.draw_small(dev2)
+# Drawer1.draw_playground(play)
+red_drawer = littlemonitor.draw_small(red_canvas)
 
-play.add_block(blo, 4, 4)
-play2.add_block(blo2, 0, 0)
+#play.add_block(blo, 4, 4)
+#play2.add_block(blo2, 0, 0)
 
-dev2.clear()
-dev.clear()
-Drawer1.draw_playground(play)
+red_canvas.clear()
+color_canvas.clear()
+#Drawer1.draw_playground(play)
 
-Drawer2.draw_playground(play2)
+red_drawer.draw_playground(red_playground)
 time.sleep(3)
 t = 0
+
 #solange noch kein gameover ist
 #wenn der block sich nicht mehr bewegen kann
 while t < 1000:
-    blo = blo3
-    blo3 = rand.get_random_block()
-    blo2 = blo3.strech_block_twice()
+    current_block = next_block
+    next_block = rand.get_random_block()
 
-    Drawer1 = drawer.draw(dev)
+    # Vorschau
+    preview_block = next_block.strech_block_twice()
+    red_playground.clear()
+    red_playground.add_block(preview_block, 0, 0)
+    red_drawer.draw_playground(red_playground)
 
-    blo.draw_block()
-    Drawer1.draw_playground(play)
-    Drawer2 = littlemonitor.draw_small(dev2)
 
-    play.add_block(blo, 4, 4)
-    play2.add_block(blo2, 0, 0)
+    # Spiel
 
-    dev2.clear()
-    dev.clear()
-    Drawer1.draw_playground(play)
+    countdown = 20
+    while countdown > 0:
+        print(countdown)
+        countdown = countdown - 1
+        linecount = 19 - countdown
+        time.sleep(0.2)
+        color_playground.add_block(current_block, 0, linecount)
+        color_drawer.draw_playground(color_playground)
+        color_playground.clear()
+    color_playground.add_block(current_block, 4, 4)
 
-    Drawer2.draw_playground(play2)
-    time.sleep(3)
     t=t+1
 #bis hier in die schleife dann...
 
 i = 0
 
 while  i<100:
-    rotater.control(g, blo)
-    play.add_block(blo, 4, 4)
-    Drawer1.draw_playground(play)
+    rotater.control(g, current_block)
+    color_playground.add_block(current_block, 4, 4)
+    color_drawer.draw_playground(color_playground)
     time.sleep(0.11)
 
 time.sleep(10)
