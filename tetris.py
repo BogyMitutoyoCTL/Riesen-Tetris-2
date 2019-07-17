6a9ea1461dad8c3175449aae12f52572dba94cad
JoelHiller <joel.david.hiller@gmail.com>
2019-07-17 10:00:47 +0200

    Tetris(v.0.2)

--- tetris.py
+++ tetris.py
@@ -1,6 +1,9 @@
 import time
 
-from luma.led_matrix.device import neopixel
+from luma.core.interface.serial import spi, noop
+
+import littlemonitor
+from luma.led_matrix.device import neopixel, max7219
 
 import random_blocks
 import playground
@@ -13,26 +16,37 @@ pygame.joystick.init()
 a = pygame.joystick.Joystick(0)
 a.init()
 
-device = neopixel(width=10, height=20, rotate=0, mapping=drawer.HAT)
+dev = neopixel(width=10, height=20, rotate=0, mapping=drawer.HAT)
+
+serial = spi(port=0, device=0, gpio=noop())
+dev2 = max7219(serial, cascaded=4, block_orientation=90,
+                 rotate=0, blocks_arranged_in_reverse_order=True)
+
 
 play = playground.Playground(20, 10)
 play.draw()
+
+play2 = playground.Playground(8, 32)
+play2.draw()
+
 
 
 rand = random_blocks.Randomblock()
 blo = rand.get_random_block()
 
-Drawer1 = drawer.draw(device)
+Drawer1 = drawer.draw(dev)
 
 blo.draw_block()
 Drawer1.draw_playground(play)
+Drawer2 = littlemonitor.draw_small(dev2)
 
 play.add_block(blo)
-Drawer1.device
+play2.add_block(blo)
 
-device.clear()
+dev2.clear()
+dev.clear()
 Drawer1.draw_playground(play)
 
-
+Drawer2.draw_playground(play2)
 
 time.sleep(10)
