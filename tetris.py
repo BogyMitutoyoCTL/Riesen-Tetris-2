6bc8a97da827fcb3f567fa097ddea1a1ec96525a
haeffema <haeffema@gmail.com>
2019-07-18 13:50:16 +0200

    Blöcke kommen wieder runter

--- tetris.py
+++ tetris.py
@@ -115,7 +115,7 @@ def run_game():
                 tim = 0.1
 
             if fadfaf.collision(color_playground, current_block, 0, linecount) == True:
-                pass
+                countdown = 0
 
             color_playground.add_block(current_block, current_block_position[0], current_block_position[1])
             color_drawer.draw_playground(color_playground)
@@ -128,6 +128,7 @@ def run_game():
             current_block_position = (current_block_position[0], current_block_position[1] + 1)
 
         t = t + 1
+        current_block_position = (current_block_position[0], current_block_position[1] - 20)
     # bis hier in die schleife dann...
 
 
