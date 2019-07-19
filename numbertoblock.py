fe7b37f42d42296c792aaa3c57974f48473781bb
Dieter Blocher <dieter.blocher@mutitoyo-ctl.de>
2019-07-19 08:29:31 +0200

    Rotate field in numbertoblock.py

--- numbertoblock.py
+++ numbertoblock.py
@@ -10,20 +10,37 @@ class NumberToBlock:
 
         blocks = []
         for number in numbers_list:
-            blocks.append(numbersforscore.NumbersForScore.number[number])
+            block = numbersforscore.NumbersForScore.number[number]
+            blocks.append(block)
 
-        field = []
-        for block in blocks:
-            field.extend(block)
+        # Create Empty Array...
+        field = NumberToBlock.create_number_field(blocks)
+
+        single_block_width = len(blocks[len(blocks) - 1])
+        for block_index in range(len(blocks)):
+            for line_index in range(len(blocks[block_index])):
+                print("LineWidth: " + str(len(blocks[block_index][line_index])))
+                for pos_index in range(len(blocks[block_index])):
+                    pos = blocks[block_index][line_index][pos_index]
+                    field_x = line_index
+                    field_y = block_index * single_block_width + pos_index
+                    field[field_x][field_y] = pos
 
         return tetris_blocks.Block([field]*4, color.BlockColor.red)
 
+    @staticmethod
+    def create_number_field(blocks):
+        single_block_width = len(blocks[len(blocks) - 1])
+        field = []
+        for line in range(single_block_width):
+            line = []
+            for line_pos in range(0, len(blocks) * single_block_width):
+                line.append(0)
+            field.append(line)
+        return field
 
     @staticmethod
     def get_list_of_single_numbers(number: int):
         numbers_list = [number % 10, number // 10 % 10, number // 100 % 10, number // 1000 % 10]
         return numbers_list
 
-
-nb = NumberToBlock()
-nb.get_block(98876)
