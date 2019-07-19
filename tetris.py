d935179096f23f79aea0ca076e0f10f143795f47
StrgV <timporgertim@gmail.com>
2019-07-19 16:36:22 +0200

    clock for tetris

--- tetris.py
+++ tetris.py
@@ -1,5 +1,6 @@
 import time
 import pygame
+import datetime
 
 import Collision
 import controller
@@ -12,6 +13,36 @@ import points
 import random_blocks
 import rgbleddrawer
 
+
+def show_clock_until_start_is_pressed(color_playground, rgg_led_drawer, red_playground, led_matrix_drawer, controller):
+    while True:
+        color_playground.clear()
+        red_playground.clear()
+        # TODO: this is a quite ugly hack, since NumberToBlock only supports 4 digit numbers
+        now = datetime.datetime.now()
+        min = now.time().minute
+        hour = now.time().hour
+        mon = now.date().month
+        today = now.date().day
+        sec = now.time().second
+        year = now.date().year % 100
+        color_playground.add_block(numbertoblock.NumberToBlock.get_block(hour*100), 0,0)
+        color_playground.add_block(numbertoblock.NumberToBlock.get_block(min*100), 0, 6)
+        color_playground.add_block(numbertoblock.NumberToBlock.get_block(sec * 100), 0, 12)
+        red_playground.add_block(numbertoblock.NumberToBlock.get_block(today*100+mon), 0, 0)
+        red_playground.add_block(numbertoblock.NumberToBlock.get_block(year*100), 21, 0)
+
+        rgg_led_drawer.draw_playground(color_playground)
+        led_matrix_drawer.draw_playground(red_playground)
+        pygame.time.Clock().tick(1)
+
+        rand = random_blocks.Randomblock()
+        next_block = rand.get_random_block()
+        result = controller.get_button_pressed(next_block,(1,1), Collision.Collision_Dedektor(), playground.Playground(20, 10))
+        if result == "Restart":
+            break
+    color_playground.clear()
+    red_playground.clear()
 
 def run_game():
     # Some stuff needed by PyGame
@@ -57,6 +88,8 @@ def run_game():
     calculator = points.Points()
 
     clock = pygame.time.Clock()  # type: pygame.time.Clock
+
+    show_clock_until_start_is_pressed(color_playground, rgg_led_drawer, red_playground, led_matrix_drawer, gamepad)
 
     game_over = False
     while not game_over:
