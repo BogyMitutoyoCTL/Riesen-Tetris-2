6f60da2f9d2627bca2b6acdf830f5e8b905661b1
Dieter Blocher <dieter.blocher@mutitoyo-ctl.de>
2019-07-19 13:36:53 +0200

    Dynamic Framerate

--- gamespeed.py
+++ gamespeed.py
@@ -1,25 +1,11 @@
+import math
+
+
 class GameSpeed:
     @staticmethod
-    def game_speed(score):
-        frame_rate = 20
-        if score >= 50:
-            frame_rate = 21
-        if score >= 100:
-            frame_rate = 22
-        if score >= 200:
-            frame_rate = 23
-        if score >= 300:
-            frame_rate = 24
-        if score >= 400:
-            frame_rate = 25
-        if score >= 500:
-            frame_rate = 26
-        if score >= 1000:
-            frame_rate = 27
-        if score >= 3000:
-            frame_rate = 28 * (0 + score/3000.0)
-            if frame_rate > 60:
-                frame_rate = 60
+    def game_speed(score, base_game_frame_rate=20):
+        factor = math.exp(score / 1000.0)
+        frame_rate = factor * base_game_frame_rate
         return frame_rate
 
 
