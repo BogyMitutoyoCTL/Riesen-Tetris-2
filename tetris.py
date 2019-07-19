ed12703be1d8c5764a08da528a845a05f4aef00a
Dieter Blocher <dieter.blocher@mutitoyo-ctl.de>
2019-07-19 14:14:56 +0200

    check scorediff

--- tetris.py
+++ tetris.py
@@ -109,7 +109,9 @@ def run_game():
                 old_score = score
                 score = check_for_full_lines(calculator, color_playground, full_line_detector, score)
                 new_score = score
-                if (new_score - old_score) == 10:
+                score_diff = new_score - old_score
+                print(score_diff)
+                if score_diff == 10:
                     red_playground.add_block(numbertoblock.NumberToBlock.get_block(10), 0, 0)
                     pygame.mixer.Sound.play(lines1_3)
                 break
