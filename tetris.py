3428875ce555a31670ebfe33fbe673921ad0e7e2
haeffema <52890607+haeffema@users.noreply.github.com>
2019-09-12 20:10:20 +0200

    Pong titlescreen 3times sec tetris 2times

--- tetris.py
+++ tetris.py
@@ -54,7 +54,7 @@ def tetris_titlescreen(color_playground, rgg_led_drawer, red_playground, led_mat
     while True:
         color_playground.clear()
         red_playground.clear()
-        # TODO: this is a quite ugly hack, since NumberToBlock only supports 4 digit number
+        # TODO: this is a quite ugly hack, since NumberToBlock only supports 4 digit numbers
         now = datetime.datetime.now()
         min = now.time().minute
         hour = now.time().hour
