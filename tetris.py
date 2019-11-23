4929a3690ff4aad09ddb5cd21a1214c044f8e288
haeffema <52890607+haeffema@users.noreply.github.com>
2019-11-23 18:47:22 +0100

    Titlescreens updated -> easier programm new

--- tetris.py
+++ tetris.py
@@ -14,7 +14,7 @@ import random_blocks
 import rgbleddrawer
 
 
-def clock_titlescreen(color_playground, rgg_led_drawer, red_playground, led_matrix_drawer, controller):
+def show_clock_until_start_is_pressed(color_playground, rgg_led_drawer, red_playground, led_matrix_drawer, controller):
     while True:
         color_playground.clear()
         red_playground.clear()
@@ -26,11 +26,11 @@ def clock_titlescreen(color_playground, rgg_led_drawer, red_playground, led_matr
         today = now.date().day
         sec = now.time().second
         year = now.date().year % 100
-        color_playground.add_block(numbertoblock.NumberToBlock.get_block(hour * 100), 0, 0)
-        color_playground.add_block(numbertoblock.NumberToBlock.get_block(min * 100), 0, 6)
+        color_playground.add_block(numbertoblock.NumberToBlock.get_block(hour*100), 0,0)
+        color_playground.add_block(numbertoblock.NumberToBlock.get_block(min*100), 0, 6)
         color_playground.add_block(numbertoblock.NumberToBlock.get_block(sec * 100), 0, 12)
-        red_playground.add_block(numbertoblock.NumberToBlock.get_block(today * 100 + mon), 0, 0)
-        red_playground.add_block(numbertoblock.NumberToBlock.get_block(year * 100), 21, 0)
+        red_playground.add_block(numbertoblock.NumberToBlock.get_block(today*100+mon), 0, 0)
+        red_playground.add_block(numbertoblock.NumberToBlock.get_block(year*100), 21, 0)
 
         rgg_led_drawer.draw_playground(color_playground)
         led_matrix_drawer.draw_playground(red_playground)
@@ -38,12 +38,12 @@ def clock_titlescreen(color_playground, rgg_led_drawer, red_playground, led_matr
 
         rand = random_blocks.Randomblock()
         next_block = rand.get_random_block()
-        result = controller.get_button_pressed(next_block, (1, 1), Collision.Collision_Dedektor(),
-                                               playground.Playground(20, 10))
+        result = controller.get_button_pressed(next_block,(1,1), Collision.Collision_Dedektor(), playground.Playground(20, 10))
         if result == "Restart":
             break
     color_playground.clear()
     red_playground.clear()
+
 def run_game():
     # Some stuff needed by PyGame
     pygame.init()
@@ -60,7 +60,7 @@ def run_game():
     break_sound = pygame.mixer.Sound('./Music/break.wav')
     lines1_3 = pygame.mixer.Sound('./Music/1.-3.lane.wav')
     line4 = pygame.mixer.Sound('./Music/4.lane.wav')
-    # pygame.mixer.music.play(-1)
+    #pygame.mixer.music.play(-1)
     pygame.mixer.music.set_volume(0.5)
     pygame.mixer.music.play(-1)
 
@@ -88,7 +88,9 @@ def run_game():
     calculator = points.Points()
 
     clock = pygame.time.Clock()  # type: pygame.time.Clock
-    clock_titlescreen(color_playground, rgg_led_drawer, red_playground, led_matrix_drawer, controller)
+
+    show_clock_until_start_is_pressed(color_playground, rgg_led_drawer, red_playground, led_matrix_drawer, gamepad)
+
     game_over = False
     while not game_over:
         pygame.mixer.Sound.play(new_block)
@@ -152,7 +154,7 @@ def run_game():
                 pygame.mixer.Sound(break_sound)
 
                 break
-            if (countdown % 10 == 0):
+            if (countdown%10 == 0):
                 current_block_position = (current_block_position[0], current_block_position[1] + 1)
 
         current_block_position = (color_playground.width // 2, 0)
