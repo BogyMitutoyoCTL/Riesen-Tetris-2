15aded22083c6f2e44e82e8b0a64c57a854ddfbe
JoelHiller <joel.david.hiller@gmail.com>
2019-12-15 16:40:34 +0100

    Paddlesteuerung

--- Tetris_Clock.py
+++
@@ -1,8 +0,0 @@
-import datetime
-import playground
-import tetris
-
-color_playground = playground.Playground(20, 10)
-
-
-print(datetime.datetime.now())
--- highscorelist.py
+++
@@ -1,3 +0,0 @@
-class Highscorelist:
-    def __init__(self):
-        pass
