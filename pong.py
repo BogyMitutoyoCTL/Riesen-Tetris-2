d21221bdc5f2c0d12286cf08f81280730884fbae
haeffema <52890607+haeffema@users.noreply.github.com>
2019-12-27 15:53:15 +0100

    malen versuch

--- pong.py
+++ pong.py
@@ -65,9 +65,10 @@ def run_game():
     score = -1
 
     #show_clock_until_start_is_pressed(color_playground, rgb_led_drawer, red_playground, led_matrix_drawer, gamepad)
+    countdown =0
 
     game_over = False
-    while True:
+    while contdown < 1000:
 
         # Prepare red_playgound to repaint...
         red_playground.clear()
@@ -76,7 +77,7 @@ def run_game():
         color_playground.add_object(objects.Objecttype.paddle_left, 1, 1)
         # draw red_playgound
         rgb_led_drawer.draw_playground(color_playground)
-
+        countdown +1
         # Spiel
     del led_matrix_drawer
     del rgb_led_drawer
