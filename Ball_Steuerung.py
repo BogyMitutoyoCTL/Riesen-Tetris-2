b3d64bb5d4b0dc28bf5ab9426160e385fe42b7cc
JoelHiller <joel.david.hiller@gmail.com>
2019-12-28 16:59:47 +0100

    Ballsteuerung

--- Ball_Steuerung.py
+++ Ball_Steuerung.py
@@ -4,22 +4,18 @@ from objects import Object
 
 
 class Ball_Steuerung:
-    def __init__(self):
-        orientation = None
 
-    def bounce(self, b1:Object,b2:Object,p:playground, cx, cy):
-        if(Collision.Collision_Dedektor.with_object(p, b1, cx, cy)==True):
-            newposition = (cx+1, cy)
-        if (Collision.Collision_Dedektor.with_object(p, b2, cx, cy) == True):
-            newposition = (cx-1, cy)
-        if(Collision.Collision_Dedektor.on_ground(p, cy)):
-            newposition = (cx, cy+1)
-        if(Collision.Collision_Dedektor.on_top(p, cy)):
-            newposition = (cx, cy-1)
+    def orientation_scanner(self, p:playground, b1:Object, b2:Object, c:Collision.Collision_Dedektor, b:Object):
+        if(Collision.Collision_Dedektor.on_top(c, p, b,b.posy) == True):
+            b.orientation_y = -b.orientation_y
+        if (Collision.Collision_Dedektor.on_ground(c, p, b, b.posy) == True):
+            b.orientation_y = -b.orientation_y
+        if (Collision.Collision_Dedektor.with_object(p, b1, b.posx, b.posy) == True):
+            b.orientation_x = -b.orientation_x
+        if (Collision.Collision_Dedektor.with_object(p, b2, b.posx, b.posy) == True):
+            b.orientation_x = -b.orientation_x
 
-        return newposition
-
-    def ingame(self, p:playground, b1:Object, b2:Object, position):
-        position = self.bounce(b1, b2, p, position[0], position[1])
-
-        return position
+    def position_calculator(self, p:playground, b1: Object, b2:Object, c:Collision.Collision_Dedektor,b:Object):
+        self.orientation_scanner(p, b1, b2, c, b)
+        b.posx = b.posx + b.orientation_x
+        b.posy = b.posy + b.orientation_y
--- objects.py
+++ objects.py
@@ -15,6 +15,10 @@ class Object:
         self.color = color
         self.orientation = 0
         self.field_with_rotations = field
+        self.orientation_x = 0
+        self.orientation_y = 0
+        self.posx = 0
+        self.posy = 0
 
     def height(self):
         return len(self.get_field())
