f346ee55375fc89d1cd820a14c2e6d41a3a70f04
haeffema <52890607+haeffema@users.noreply.github.com>
2019-12-30 15:51:55 +0100

    malen versuch

--- playground.py
+++ playground.py
@@ -18,10 +18,10 @@ class Playground:
             for x in range(self.width):
                 self.set_pixel(x, y, (0, 0, 0))
 
-    def add_object(self, object: objects.Object, columns_right=0, lines_down=0):
-        for y_of_block in range(object.height):
-            for x_of_block in range(object.width):
-                ispixel = object.get_field()[y_of_block][x_of_block]
+    def add_object(self, Object: objects.Object, columns_right=0, lines_down=0):
+        for y_of_block in range(Object.height):
+            for x_of_block in range(Object.width):
+                ispixel = Object.get_field()[y_of_block][x_of_block]
                 if ispixel > 0:
                     if not self.is_inside_field(x_of_block + columns_right, y_of_block + lines_down):
                         break
