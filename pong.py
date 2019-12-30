f3c8ec767001caf284b4d2a8e73e5c3b158a6f5c
haeffema <52890607+haeffema@users.noreply.github.com>
2019-12-30 16:20:13 +0100

    max macht alles alleine schau nur history

--- pong.py
+++ pong.py
@@ -71,7 +71,7 @@ def run_game():
         #red_playground.clear()
         #color_playground.clear()
         # Add preview block to red_playgound
-        color_playground.add_object(objects.object_list[0], 1, 1)
+        color_playground.add_object(objects.Object.object_list[0], 1, 1)
         # draw red_playgound
         rgb_led_drawer.draw_playground(color_playground)
 
