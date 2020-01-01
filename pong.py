e4c008c2c2b12b52444a2fbf5d232ed471d63643
haeffema <52890607+haeffema@users.noreply.github.com>
2020-01-01 17:08:56 +0100

    variablen für objekte

--- pong.py
+++ pong.py
@@ -81,8 +81,8 @@ def run_game():
         # color_playground.clear()
         # Add preview block to red_playgound
         color_playground.add_object(ball, 1, 1)
-        color_playground.add_object(paddle_left, 0, 7)
-        color_playground.add_object(paddle_left, 10, 7)
+        color_playground.add_object(paddle_left, 2, 2)
+        color_playground.add_object(paddle_left, 5, 5)
         # draw red_playgound
         rgb_led_drawer.draw_playground(color_playground)
         # Spiel
