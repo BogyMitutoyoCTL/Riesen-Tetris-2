a5aaa5bb5e20653270bcdb525041833bef844b4e
haeffema <52890607+haeffema@users.noreply.github.com>
2020-01-02 13:56:38 +0100

    titlescreen try lol

--- pong.py
+++ pong.py
@@ -44,12 +44,11 @@ def run_game():
     tetristitlescreen = False
     while tetrisplay == False or pongplay == False:
         while clock == True:
-            titlescreens.show_clock_until_start_is_pressed(color_playground, rgb_led_drawer, red_playground,
-                                                                    led_matrix_drawer, gamepad)
+            show_clock_until_start_is_pressed(color_playground, rgb_led_drawer, red_playground,led_matrix_drawer, gamepad)
         while pongtitlescreen == True:
-            titlescreens.show_pong_titlescreen()
+            show_pong_titlescreen()
         while tetristitlescreen == True:
-            titlescreens.show_tetris_titlescreen()
+            show_tetris_titlescreen()
     if tetrisplay == True:
         tetris()
     if pongplay == True:
@@ -148,7 +147,114 @@ def pong():
     pygame.quit()
 def tetris():
     pygame.init()
+def show_clock_until_start_is_pressed(color_playground, rgg_led_drawer, red_playground, led_matrix_drawer,
+                                      controller):
+    while True:
+        color_playground.clear()
+        red_playground.clear()
+        # TODO: this is a quite ugly hack, since NumberToBlock only supports 4 digit numbers
+        now = datetime.datetime.now()
+        min = now.time().minute
+        hour = now.time().hour
+        mon = now.date().month
+        today = now.date().day
+        sec = now.time().second
+        year = now.date().year % 100
+        color_playground.add_block(numbertoblock.NumberToBlock.get_block(hour * 100), 0, 0)
+        color_playground.add_block(numbertoblock.NumberToBlock.get_block(min * 100), 0, 6)
+        color_playground.add_block(numbertoblock.NumberToBlock.get_block(sec * 100), 0, 12)
+        red_playground.add_block(numbertoblock.NumberToBlock.get_block(today * 100 + mon), 0, 0)
+        red_playground.add_block(numbertoblock.NumberToBlock.get_block(year * 100), 21, 0)
 
+        rgg_led_drawer.draw_playground(color_playground)
+        led_matrix_drawer.draw_playground(red_playground)
+        pygame.time.Clock().tick(1)
+
+        # rand = random_blocks.Randomblock()
+        # next_block = rand.get_random_block()
+        result = controller.get_button_pressed()
+        if result == "Left Title":
+            run_game().pongtitlescreen = True
+            run_game().clock = False
+        if result == "Right Title":
+            run_game().tetristitlescreen = True
+            run_game().clock = False
+    color_playground.clear()
+    red_playground.clear()
+
+def show_pong_titlescreen(color_playground, rgg_led_drawer, red_playground, led_matrix_drawer,controller):
+    while True:
+        color_playground.clear()
+        red_playground.clear()
+        # TODO: this is a quite ugly hack, since NumberToBlock only supports 4 digit numbers
+        now = datetime.datetime.now()
+        min = now.time().minute
+        hour = now.time().hour
+        mon = now.date().month
+        today = now.date().day
+        sec = now.time().second
+        year = now.date().year % 100
+        color_playground.add_block(numbertoblock.NumberToBlock.get_block(hour * 100), 0, 0)
+        color_playground.add_block(numbertoblock.NumberToBlock.get_block(min * 100), 0, 6)
+        color_playground.add_block(numbertoblock.NumberToBlock.get_block(sec * 100), 0, 12)
+        red_playground.add_block(numbertoblock.NumberToBlock.get_block(today * 100 + mon), 0, 0)
+        red_playground.add_block(numbertoblock.NumberToBlock.get_block(year * 100), 21, 0)
+
+        rgg_led_drawer.draw_playground(color_playground)
+        led_matrix_drawer.draw_playground(red_playground)
+        pygame.time.Clock().tick(1)
+
+        # rand = random_blocks.Randomblock()
+        # next_block = rand.get_random_block()
+        result = controller.get_button_pressed()
+        if result == "Left Title":
+            run_game().tetristitlescreen = True
+            run_game().pongtitlescreen = False
+        if result == "Right Title":
+            run_game().clock = True
+            run_game().pongtitlescreen = False
+        if result == "Restart":
+            run_game().pongplay = True
+            run_game().pongtitlescreen = False
+    color_playground.clear()
+    red_playground.clear()
+
+def show_tetris_titlescreen(color_playground, rgg_led_drawer, red_playground, led_matrix_drawer, controller):
+    while True:
+        color_playground.clear()
+        red_playground.clear()
+        # TODO: this is a quite ugly hack, since NumberToBlock only supports 4 digit numbers
+        now = datetime.datetime.now()
+        min = now.time().minute
+        hour = now.time().hour
+        mon = now.date().month
+        today = now.date().day
+        sec = now.time().second
+        year = now.date().year % 100
+        color_playground.add_block(numbertoblock.NumberToBlock.get_block(hour * 100), 0, 0)
+        color_playground.add_block(numbertoblock.NumberToBlock.get_block(min * 100), 0, 6)
+        color_playground.add_block(numbertoblock.NumberToBlock.get_block(sec * 100), 0, 12)
+        red_playground.add_block(numbertoblock.NumberToBlock.get_block(today * 100 + mon), 0, 0)
+        red_playground.add_block(numbertoblock.NumberToBlock.get_block(year * 100), 21, 0)
+
+        rgg_led_drawer.draw_playground(color_playground)
+        led_matrix_drawer.draw_playground(red_playground)
+        pygame.time.Clock().tick(1)
+
+        # rand = random_blocks.Randomblock()
+        # next_block = rand.get_random_block()
+        result = controller.get_button_pressed()
+        if result == "Left Title":
+            run_game().clock = True
+            run_game().tetristitlescreen = False
+        if result == "Right Title":
+            run_game().pongtitlescreen = True
+            run_game().tetristitlescreen = False
+        if result == "Restart":
+            run_game().tetrisplay = True
+            run_game().tetristitlescreen = False
+    color_playground.clear()
+    red_playground.clear()
 def block_is_above_beginning(block, line):
     for y in range(block.height):
         for x in range(block.width):
