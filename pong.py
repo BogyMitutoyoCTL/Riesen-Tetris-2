e009034c246f837497b024e1ea22fa222956f097
haeffema <52890607+haeffema@users.noreply.github.com>
2020-01-03 21:44:16 +0100

    Versuche mit Bot und der Schwierigkeit

--- pong.py
+++ pong.py
@@ -227,13 +227,13 @@ def round(b: object, b1: object, b2: object, c: Collision.Collision_Dedektor, p:
 
 def bot_steuerung(s:object,b:object):
     fail = random.random()
-    if fail > 0:
-        if s.posx-b.posx >= 0:
-            if s.posx - 1 >= 0 and s.posx - 1 <= 7:
-                s.posx -= 1
-        if s.posx-b.posx-1 < 0:
-            if s.posx + 1 >= 0 and s.posx + 1 <= 7:
-                s.posx += 1
+   #if fail > 0:
+    if s.posx-b.posx >= 0:
+        if s.posx > 0:
+            s.posx -= 1
+    if s.posx-b.posx < 0:
+        if s.posx < 7:
+            s.posx += 1
 
 if __name__ == "__main__":
     while True:
