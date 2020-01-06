3abc2ee83571030e68b7c125cb8a1120d3ee175f
JoelHiller <joel.david.hiller@gmail.com>
2020-01-06 19:31:32 +0100

    Spiel gefixed

--- pong.py
+++ pong.py
@@ -15,17 +15,35 @@ import random
 import Ball_Steuerung
 
 
-def titlescreen(rgb:rgbleddrawer.RgbLedDrawer(), playground:object_playground.Playground, ball, paddle_top, paddle_bot):
-    paddle_top.posx = 4
-    paddle_bot.posx = 4
-    paddle_top.posy = 0
-    paddle_bot.posy = 19
-    ball.posx = 5
+def show_clock_until_start_is_pressed(color_playground, rgg_led_drawer, red_playground, led_matrix_drawer, controller):
+    while True:
+        color_playground.clear()
+        red_playground.clear()
+        # TODO: this is a quite ugly hack, since NumberToBlock only supports 4 digit numbers.
+        now = datetime.datetime.now()
+        min = now.time().minute
+        hour = now.time().hour
+        mon = now.date().month
+        today = now.date().day
+        sec = now.time().second
+        year = now.date().year % 100
+        color_playground.add_block(numbertoblock.NumberToBlock.get_block(hour * 100), 0, 0)
+        color_playground.add_block(numbertoblock.NumberToBlock.get_block(min * 100), 0, 6)
+        color_playground.add_block(numbertoblock.NumberToBlock.get_block(sec * 100), 0, 12)
+        red_playground.add_block(numbertoblock.NumberToBlock.get_block(today * 100 + mon), 0, 0)
+        red_playground.add_block(numbertoblock.NumberToBlock.get_block(year * 100), 21, 0)
 
-    playground.add_object(paddle_top, paddle_top.posx, paddle_top.posy)
-    playground.add_object(paddle_bot, paddle_bot.posx, paddle_bot.posy)
-    ball.posy = 7
-    playground.add_object(ball, ball.posx, ball.posy)
+        rgg_led_drawer.draw_playground(color_playground)
+        led_matrix_drawer.draw_playground(red_playground)
+        pygame.time.Clock().tick(1)
+
+        # rand = random_blocks.Randomblock()
+        # next_block = rand.get_random_block()
+        result = controller.get_button_pressed()
+        if result == "Restart":
+            break
+    color_playground.clear()
+    red_playground.clear()
 
 def run_game():
     # variables for objects
@@ -47,11 +65,11 @@ def run_game():
     joystick = pygame.joystick.Joystick(0)
     joystick.init()
 
-    joystick2 = pygame.joystick.Joystick(1)
-    joystick2.init()
+    #joystick2 = pygame.joystick.Joystick(1)
+    #joystick2.init()
 
     gamepad = controller.Controller(joystick)
-    gamepad2 = controller.Controller(joystick2)
+    #gamepad2 = controller.Controller(joystick2)
 
     # drawer for playfield
     rgb_led_drawer = rgbleddrawer.RgbLedDrawer()
@@ -64,7 +82,7 @@ def run_game():
     red_playground = object_playground.Playground(8, 32)
     color_playground.clear()
 
-    titlescreen(rgb_led_drawer, color_playground, ball, paddle_top, paddle_bot)
+    show_clock_until_start_is_pressed(color_playground, rgb_led_drawer, red_playground, led_matrix_drawer, gamepad)
     # Prepare red_playgound to repaint...
     # red_playground.clear()
     # color_playground.clear()
