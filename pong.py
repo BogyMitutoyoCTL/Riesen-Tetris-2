c392594761e21a68fae1d5321ddde44b1f335320
JoelHiller <joel.david.hiller@gmail.com>
2020-01-06 17:19:14 +0100

    Titelscreens

--- pong.py
+++ pong.py
@@ -15,36 +15,17 @@ import random
 import Ball_Steuerung
 
 
-def show_clock_until_start_is_pressed(color_playground, rgg_led_drawer, red_playground, led_matrix_drawer, controller):
-    while True:
-        color_playground.clear()
-        red_playground.clear()
-        # TODO: this is a quite ugly hack, since NumberToBlock only supports 4 digit numbers.
-        now = datetime.datetime.now()
-        min = now.time().minute
-        hour = now.time().hour
-        mon = now.date().month
-        today = now.date().day
-        sec = now.time().second
-        year = now.date().year % 100
-        color_playground.add_block(numbertoblock.NumberToBlock.get_block(hour * 100), 0, 0)
-        color_playground.add_block(numbertoblock.NumberToBlock.get_block(min * 100), 0, 6)
-        color_playground.add_block(numbertoblock.NumberToBlock.get_block(sec * 100), 0, 12)
-        red_playground.add_block(numbertoblock.NumberToBlock.get_block(today * 100 + mon), 0, 0)
-        red_playground.add_block(numbertoblock.NumberToBlock.get_block(year * 100), 21, 0)
+def titlescreen(rgb:rgbleddrawer.RgbLedDrawer(), playground:playground.Playground, ball, paddle_top, paddle_bot):
+    paddle_top.posx = 4
+    paddle_bot.posx = 4
+    paddle_top.posy = 0
+    paddle_bot.posy = 19
+    ball.posx = 5
 
-        rgg_led_drawer.draw_playground(color_playground)
-        led_matrix_drawer.draw_playground(red_playground)
-        pygame.time.Clock().tick(1)
-
-        # rand = random_blocks.Randomblock()
-        # next_block = rand.get_random_block()
-        result = controller.get_button_pressed()
-        if result == "Restart":
-            break
-    color_playground.clear()
-    red_playground.clear()
-
+    playground.add_object(paddle_top, paddle_top.posx, paddle_top.posy)
+    playground.add_object(paddle_bot, paddle_bot.posx, paddle_bot.posy)
+    ball.posy = 7
+    playground.add_object(ball, ball.posx, ball.posy)
 
 def run_game():
     # variables for objects
@@ -83,7 +64,7 @@ def run_game():
     red_playground = playground.Playground(8, 32)
     color_playground.clear()
 
-    show_clock_until_start_is_pressed(color_playground, rgb_led_drawer, red_playground, led_matrix_drawer, gamepad)
+    titlescreen(rgb_led_drawer, color_playground, ball, paddle_top, paddle_bot)
     # Prepare red_playgound to repaint...
     # red_playground.clear()
     # color_playground.clear()
@@ -148,9 +129,7 @@ def run_game():
                 ball.orientation_x = -ball.orientation_x
                 pygame.mixer.Sound.play(abpraller_sound)
 
-            if Collision.Collision_Dedektor.with_object(Collision.Collision_Dedektor, color_playground, ball,
-                                                        ball.posx + ball.orientation_x,
-                                                        ball.posy + ball.orientation_y) == True:
+            if Collision.Collision_Dedektor.with_object(Collision.Collision_Dedektor, color_playground, ball, ball.posx + ball.orientation_x, ball.posy + ball.orientation_y) == True:
                 Runde += 1
                 ball.orientation_y = -ball.orientation_y
                 pygame.mixer.Sound.play(abpraller_sound)
@@ -192,7 +171,6 @@ def run_game():
             pygame.time.wait(3000)
             game_over = True
             break
-        pygame.mixer.Sound.play(gameover_sound)
         pygame.time.wait(3000)
 
         color_playground.clear()
